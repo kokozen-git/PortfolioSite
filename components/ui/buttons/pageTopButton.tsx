96ad3f61afebc8e16d124cd6be0b5e4914dce4ca
kokozen-git <nkgeek.main@gmail.com>
2026-08-29 17:14:26 +0900

    修正6

--- components/ui/buttons/pageTopButton.tsx
+++ components/ui/buttons/pageTopButton.tsx
@@ -8,16 +8,25 @@ export default function PageTopButton() {
     window.scrollTo({ top: 0, behavior: "smooth" });
   }
 
+  const [isTouched, setIsTouched] = useState(false);
+
   return (
     <button
       type="button"
       onClick={scrollToTop}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
+      onTouchStart={() => setIsTouched(true)}
+      onTouchEnd={() => setIsTouched(false)}
       className="group flex items-center gap-4 text-white hover:cursor-pointer w-fit h-fit"
     >
       <span
-        className="flex h-10 w-7 items-center justify-center rounded-full border border-white/60 transition-colors duration-300 group-hover:bg-white group-hover:text-(--mainColor)"
+        className={`
+          flex h-10 w-7 items-center justify-center rounded-full 
+          border border-white/60 transition-colors duration-300 
+          group-hover:bg-white group-hover:text-(--mainColor)
+          ${isTouched ? "group-hover:bg-white group-hover:text-(--mainColor)" : "bg-white"} 
+        `}
       >
         <svg
           width="20"
@@ -28,7 +37,10 @@ export default function PageTopButton() {
           strokeWidth="2"
           strokeLinecap="round"
           strokeLinejoin="round"
-          className={isHovered ? "animate-arrow-bounce-up" : ""}
+          className={`
+            ${isHovered ? "animate-arrow-bounce-up" : "" }
+            ${isTouched ? "animate-arrow-bounce-up" : "" }
+          `} 
         >
           <line x1="12" y1="19" x2="12" y2="5" />
           <polyline points="5 12 12 5 19 12" />
