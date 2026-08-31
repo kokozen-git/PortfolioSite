5eea230fdfdedc9571815601b2ac1aec3f253917
kokozen-git <nkgeek.main@gmail.com>
2026-08-31 13:24:52 +0900

    微細な修正

--- components/layout/blog/BlogPostList.tsx
+++ components/layout/blog/BlogPostList.tsx
@@ -28,9 +28,9 @@ export default function BlogPostList({ posts }: { posts: Post[] }) {
           onTouchEnd={() => setIsTouched(false)}
 
           className={`
-            md:h-38 rounded-xl overflow-hidden md:overflow-visible 
-            border-[var(--mainColor)]/20 md:shadow-none md:border-none 
-            transition-shadow
+            sm:h-38 rounded-xl overflow-hidden sm:overflow-visible 
+            border-[var(--mainColor)]/20 sm:shadow-none sm:border-none 
+            transition-shadow w-full
             ${isTouched ? "shadow-md" : "shadow-sm"} 
           `}
 
@@ -39,10 +39,10 @@ export default function BlogPostList({ posts }: { posts: Post[] }) {
             href={post.link}
             target="_blank"
             rel="noopener noreferrer"
-            className="md:flex h-full overflow-hidden rounded-xl md:border border-[var(--mainColor)]/20 bg-white shadow-sm transition-shadow hover:shadow-md"
+            className="sm:flex h-full overflow-hidden rounded-xl sm:border border-[var(--mainColor)]/20 bg-white shadow-sm transition-shadow hover:shadow-md"
           >
             {post.thumbnail && (
-              <div className="relative md:w-48 flex-none aspect-video">
+              <div className="relative sm:w-48 flex-none aspect-video">
                 <Image
                   src={post.thumbnail}
                   alt={post.title}
@@ -52,7 +52,7 @@ export default function BlogPostList({ posts }: { posts: Post[] }) {
                 />
               </div>
             )}
-            <div className="p-3 md:p-4 md:flex-initial">
+            <div className="p-3 sm:p-4 md:flex-initial">
               <h3 className="text-lg font-bold tracking-wide line-clamp-1">
                 {post.title}
               </h3>
